ba60c216f8dcf6e4dc48407b06aa88182ce497b1
Linead96 <danielgarboza@yahoo.com>
2019-05-25 11:31:52 +0800

    Added route for Posts component in Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import './Blog.css';
-import Posts from './Posts/Posts';
+import "./Blog.css";
+import Posts from "./Posts/Posts";
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
 
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
-                        </ul>
-                    </nav>
-                </header>
-                <Posts />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/" exact component={Posts} />
+      </div>
+    );
+  }
 }
 
 export default Blog;
