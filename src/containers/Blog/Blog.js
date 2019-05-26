65078185b245e13749267af0ac379702db6664d6
Linead96 <danielgarboza@yahoo.com>
2019-05-26 13:10:25 +0800

    Added nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import { Route, NavLink, Switch } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
   state = {
@@ -21,7 +20,7 @@ class Blog extends Component {
             <ul>
               <li>
                 <NavLink
-                  to="/"
+                  to="/posts"
                   exact
                   activeClassName="my-active"
                   activeStyle={{
@@ -29,7 +28,7 @@ class Blog extends Component {
                     textDecoration: "underline"
                   }}
                 >
-                  Home
+                  Posts
                 </NavLink>
               </li>
               <li>
@@ -46,10 +45,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact component={Posts} />
-        <Switch>  
-          <Route path="/new-post" component={NewPost} />
-          <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} /> 
+          <Route path="/posts" component={Posts} /> 
         </Switch>
       </div>
     );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import axios from "../../../axios";
+import { Route } from "react-router-dom";
 
 import Post from "../../../components/Post/Post";
 import "./Posts.css";
+import FullPost from "../FullPost/FullPost";
 
 class Posts extends Component {
   state = {
@@ -31,8 +33,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler = id => {
-    this.props.history.push({pathname: '/' + id});
-    // this.props.history.push('/' + id);
+    // this.props.history.push({ pathname: "/" + id });
+    this.props.history.push('/posts/' + id);
   };
 
   render() {
@@ -41,17 +43,22 @@ class Posts extends Component {
       posts = this.state.posts.map(post => {
         return (
           // <Link to={"/" + post.id} key={post.id}>
-            <Post
-              key={post.id}
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
           // </Link>
         );
       });
     }
-    return <section className="Posts">{posts}</section>;
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route path={this.props.match.url + "/:id"} exact component={FullPost} />
+      </div>
+    );
   }
 }
 
