615c7371018841b3048c273f6f67c2cd3b8f2bb5
Linead96 <danielgarboza@yahoo.com>
2019-05-26 11:55:22 +0800

    Added switch component in Blog.js

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -11,7 +11,10 @@ class NewPost extends Component {
   };
 
   componentDidMount() {
-    console.log(this.props);
+    // const query = new URLSearchParams(this.props.location.search);
+    // for (let param of query.entries()) {
+    //   console.log("newpost", param);
+    // }
   }
 
   postDataHandler = () => {
